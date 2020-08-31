ecba92984b0568bc3ceecc41f21c48f59e58ffec
NourhanEl-Khodary <36134694+NourhanEl-khodary@users.noreply.github.com>
2020-08-31 21:07:13 +0200

    correcting the name of the y axis

--- plot3.R
+++ plot3.R
@@ -12,7 +12,7 @@ reqData$DateTime <- strptime(paste(reqData$Date, reqData$Time),                 
 png("plot3.png", width = 480, height = 480)                                        ## Open a png graphic device with width 480 and height 480
 par(mar = c(2.5,5,2,2))                                                            ## Set the margins for the plot graph
 plot(reqData$DateTime, reqData$Sub_metering_1, type = "l",                         ## Plot on the x-axis the time and energy the y-axis sub metering (1) and 
-     ylab = "Enery sub metering")                                                  ## the type line
+     ylab = "Energy sub metering")                                                  ## the type line
 lines(reqData$DateTime, reqData$Sub_metering_2, col = "red")                       ## Add the line for sub metering 2 with color red
 lines(reqData$DateTime, reqData$Sub_metering_3, col = "blue")                      ## Add the line for sub metering 3 with color blue
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")## Add legend to the plot
