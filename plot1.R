e978531aa3420f4fb56d01fc52974d9d89b137e7
NourhanEl-Khodary <36134694+NourhanEl-khodary@users.noreply.github.com>
2020-08-31 19:59:01 +0200

    Updating plot 1 code

--- plot1.R
+++ plot1.R
@@ -4,11 +4,12 @@
 library(lubridate)
 
 
-data <- read.delim("household_power_consumption.txt", sep = ";", na.strings = "?") ## Reading the data from the data txt file
+data <- read.table("household_power_consumption.txt", sep = ";", 
+                   header = TRUE, na.strings = "?", check.names = FALSE, 
+                   stringsAsFactors = FALSE, comment.char = "", quote = '\"')      ## Reading the data from the data txt file
 reqData <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")                   ## Extracting the required data which dates are (1/2/2007 and 2/2/2007)
 reqData[,"Date"] <- dmy(reqData[,"Date"])                                          ## Convert the date from string to date object
 reqData[,"Time"] <- hms(reqData[,"Time"])                                          ## Convert the date from string to time object
-reqData[,"Global_active_power"] <- as.numeric(reqData[,"Global_active_power"])     ## Convert the date from string to numeric object
 png("plot1.png", width = 480, height = 480)                                        ## Open a png graphic device with width 480 and height 480
 hist(reqData$Global_active_power, breaks = 12, col = "red",                        ## Create the histogram for global active power, set the breaks to 12, colour as red
      xlab = "Global Active Power (kilowatts)", main = "Global Active Power")       ## label the x-axis and put a title to the plot
